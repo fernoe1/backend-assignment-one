7f4e7ffcb4e6e4575425049b1c805aed58ee179d
fernoe1 <krutoytemirlan2007@mail.ru>
2025-12-15 16:20:28 +0600

    feat: get criteria

--- back/server.js
+++ back/server.js
@@ -1,10 +1,16 @@
 import express from "express";
 import cors from "cors";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use(express.static(__dirname));
 
 app.post("/calculate-bmi", (req, res) => {
     const { height, weight } = req.body;
@@ -12,6 +18,10 @@ app.post("/calculate-bmi", (req, res) => {
     const bmi = (weight / ((height / 100) ** 2)).toFixed(2);
 
     res.json({ bmi });
+});
+
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "index.html"));
 });
 
 const PORT = 3000;
